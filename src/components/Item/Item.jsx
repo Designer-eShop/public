008e48166f319e6895dd73c99a878b03aa8f4c35
Dovydas <dodefke@gmail.com>
2020-12-16 20:51:38 +0200

    Added pictures to assets

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Section } from "../";
+import * as S from "./Item.style";
+
+function Item() {
+  return (
+    <Section fullWidth>
+      <S.Container>Hello</S.Container>
+    </Section>
+  );
+}
+
+export default Item;
