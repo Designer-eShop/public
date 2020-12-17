21346597fb3cc84771d1636f903248619d710c04
Dovydas <dodefke@gmail.com>
2020-12-17 20:37:35 +0200

    Added female clothes page

---
+++ src/pages/ForHer/ForHer.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./ForHer.style";
+import { Hero, Section, Card } from "../../components";
+import HeroImage from "../../assets/woman-hero-image.png";
+
+function ForHer() {
+  const [clothes, setClothes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/clothes")
+      .then((res) => res.json())
+      .then((data) => setClothes(data));
+  }, []);
+  return (
+    <>
+      <Hero
+        bgimg={HeroImage}
+        title="SHOP FOR WOMAN"
+        slogan="SUDDENLY OBSESSED"
+      />
+      <Section>
+        <S.FlexBlock>
+          <Card array={clothes.filter((g) => g.gender === 0)} />
+        </S.FlexBlock>
+      </Section>
+    </>
+  );
+}
+
+export default ForHer;
