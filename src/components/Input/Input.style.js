10d3b8badea74918f54c9855f2b9275f2aebf11d
Dovydas <dodefke@gmail.com>
2020-12-17 20:42:38 +0200

    Basic input field style fix

--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -2,7 +2,13 @@ import styled from "styled-components";
 
 export const Input = styled.input`
   width: 100%;
+  height: 40px;
   margin: 5px 0;
-  padding: 5px;
+  padding: 8px 12px;
   box-sizing: border-box;
+  border: 2px solid #383c43;
+  font-family: Roboto;
+  font-size: 16px;
+  line-height: 24px;
+  margin-bottom: 24px;
 `;
