fe6fa0a35349bb8606db21bf930975a595aa02cf
Dovydas <dodefke@gmail.com>
2020-12-17 20:37:50 +0200

    Added login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useContext } from "react";
+import { Form, Section } from "../../components";
+import { AuthContext } from "../../context/AuthContext";
+import { useHistory } from "react-router-dom";
+import * as S from "./Login.style";
+
+function getToken(email, password, auth, history) {
+  fetch(`http://localhost:8080/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      history.push("/");
+      auth.setState("Bearer " + data.token);
+    });
+}
+
+function Home() {
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  console.log(auth);
+
+  return (
+    <>
+      <Section>
+        <S.Container>
+          <S.Title>Login</S.Title>
+          <Form
+            forgot
+            register
+            onSubmit={(e) => {
+              e.preventDefault();
+              getToken(email, password, auth, history);
+            }}
+            buttonName="Login"
+            inputs={[
+              {
+                id: 1,
+                inputChange: (e) => setEmail(e.target.value),
+                type: "email",
+                placeholder: "Type your email address",
+                label: "Email address",
+              },
+              {
+                id: 2,
+                inputChange: (e) => setPassword(e.target.value),
+                type: "password",
+                placeholder: "Type your password",
+                label: "Password",
+              },
+            ]}
+          ></Form>
+        </S.Container>
+      </Section>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login/Login.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InputBlock = styled.div``;
+
+export const Title = styled.div`
+  font-size: 64px;
+  line-height: 77px;
+  text-align: center;
+  padding-bottom: 64px;
+  color: #383c43;
+`;
+
+export const Container = styled.div`
+  width: 290px;
+  margin: 0 auto;
+  padding: 128px 0px;
+`;
