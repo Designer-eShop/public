a911db1edc1b9ce0c9a920d0154337fdef614a03
Dovydas <dodefke@gmail.com>
2020-12-17 20:42:07 +0200

    Form component edit for login and register pages

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,19 +3,38 @@ import * as S from "./Form.style";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 
-function Form({ onSubmit, inputs }) {
+function Form({ onSubmit, inputs, buttonName, forgot, register, loginNot }) {
   return (
     <S.Form onSubmit={onSubmit}>
       {inputs &&
         inputs.map((item) => (
-          <Input
-            key={item.id}
-            inputChange={item.inputChange}
-            type={item.type}
-            placeholder={item.placeholder}
-          />
+          <S.Label key={item.id}>
+            {item.label}
+            <Input
+              inputChange={item.inputChange}
+              type={item.type}
+              placeholder={item.placeholder}
+            />
+          </S.Label>
         ))}
-      <Button type="submit">Submit</Button>
+      {forgot && (
+        <S.Forgot>
+          <S.StyledLink to="#">Forgot password?</S.StyledLink>
+        </S.Forgot>
+      )}
+      <Button type="submit">{buttonName}</Button>
+      {register && (
+        <S.Register>
+          Don’t have an account yet?{" "}
+          <S.StyledLink to="/register">Sign Up</S.StyledLink>
+        </S.Register>
+      )}
+      {loginNot && (
+        <S.LoginNot>
+          Already have an account?{" "}
+          <S.StyledLink to="/login">Log in</S.StyledLink>
+        </S.LoginNot>
+      )}
     </S.Form>
   );
 }
--- src/components/Form/Form.style.js
+++ src/components/Form/Form.style.js
@@ -1,3 +1,40 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Form = styled.form``;
+
+export const Label = styled.label`
+  font-size: 24px;
+  line-height: 24px;
+  color: #383c43;
+`;
+
+export const Forgot = styled.div`
+  font-family: Roboto;
+  font-size: 12px;
+  line-height: 24px;
+  margin-top: -20px;
+  text-align: right;
+  color: #383c43;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #383c43;
+`;
+
+export const Register = styled.div`
+  font-family: Roboto;
+  font-size: 12px;
+  line-height: 24px;
+  margin-top: 20px;
+  color: #383c43;
+`;
+
+export const LoginNot = styled.div`
+  font-family: Roboto;
+  font-size: 12px;
+  line-height: 24px;
+  margin-top: 20px;
+  color: #383c43;
+`;
