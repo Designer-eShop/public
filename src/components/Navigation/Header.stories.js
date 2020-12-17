2694d400967b4c2d35809cd2ed0e6e5833dcf7ad
Dovydas <dodefke@gmail.com>
2020-12-17 20:44:51 +0200

    removed storybook comp

--- src/components/Navigation/Header.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import Navigation from "./Navigation";
-import StoryRouter from "storybook-react-router";
-
-storiesOf("Navigation", module)
-  .addDecorator(StoryRouter())
-  .add("Navigation", () => <Navigation />);
