da763fcf7fb98b4997d361fc5f0e4bd6f5477e8b
Dovydas <dodefke@gmail.com>
2020-12-17 20:44:30 +0200

    Added lazy loading links to Routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,9 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navigation, Loading, PrivateRoute, Footer } from "./components";
 
 const HomeLazy = lazy(() => import("./pages/Home/Home"));
-const AboutLazy = lazy(() => import("./pages/About/About"));
 const SecretLazy = lazy(() => import("./pages/Secret/Secret"));
 const ProductLazy = lazy(() => import("./pages/Product/Product"));
+const LoginLazy = lazy(() => import("./pages/Login/Login"));
+const RegisterLazy = lazy(() => import("./pages/Register/Register"));
+const ForHerLazy = lazy(() => import("./pages/ForHer/ForHer"));
+const ForHimLazy = lazy(() => import("./pages/ForHim/ForHim"));
+const CartLazy = lazy(() => import("./pages/Cart/Cart"));
 
 function Routes() {
   return (
@@ -16,7 +20,11 @@ function Routes() {
           <Route exact path="/" component={HomeLazy} />
           <Route exact path="/clothes/:id" component={ProductLazy} />
           <PrivateRoute exact path="/secret" component={SecretLazy} />
-          <Route exact path="/about" component={AboutLazy} />
+          <Route exact path="/login" component={LoginLazy} />
+          <Route exact path="/register" component={RegisterLazy} />
+          <Route exact path="/forher" component={ForHerLazy} />
+          <Route exact path="/forhim" component={ForHimLazy} />
+          <Route exact path="/cart" component={CartLazy} />
         </Switch>
       </Suspense>
       <Footer />
