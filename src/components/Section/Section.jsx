174b21ad5df72e65efd2943d09bf55ed131ce59f
Dovydas <dodefke@gmail.com>
2020-12-17 20:38:31 +0200

    Removed not needed code for section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import * as S from "./Section.style";
 
-function Section({ children, fullWidth, background, maxWidth, contentWidth }) {
+function Section({ children, fullWidth, background, maxWidth }) {
   return (
     <S.Section background={background} maxWidth={maxWidth}>
-      {fullWidth ? (
-        children
-      ) : contentWidth ? (
-        <S.SectionBlock>{children}</S.SectionBlock>
-      ) : (
-        <S.Wrapper>{children}</S.Wrapper>
-      )}
+      {fullWidth ? children : <S.Wrapper>{children}</S.Wrapper>}
     </S.Section>
   );
 }
--- src/components/Section/Section.style.js
+++ src/components/Section/Section.style.js
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-  max-width: 1440px;
   margin: 0 auto;
 `;
 
 export const Wrapper = styled.section`
-  max-width: 768px;
-  padding: 1em 2em;
+  @media only screen and (max-width: 600px) {
+    max-width: 320px;
+  }
+  @media only screen and (min-width: 600px) {
+    max-width: 600px;
+  }
+  @media only screen and (min-width: 768px) {
+    max-width: 768px;
+  }
+  @media only screen and (min-width: 1400px) {
+    max-width: 1400px;
+  }
   margin: 0 auto;
-`;
-
-export const SectionBlock = styled.section`
-  max-width: 1440px;
-  margin: 0 auto;
-  padding: 64px 10px;
 `;
