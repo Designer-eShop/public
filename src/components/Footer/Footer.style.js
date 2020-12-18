7128a422afeffa804ccae1693196976def7a66f3
Dovydas <dodefke@gmail.com>
2020-12-18 21:50:40 +0200

    Responsive footer

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -12,54 +12,47 @@ export const Header = styled.div`
   @media (min-width: 1400px) {
     width: 1400px;
   }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 32px 0px 64px 0px;
+    height: 251px;
+  }
+  padding: 0 20px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   height: 144px;
-  padding: 32px 20px 64px;
   box-sizing: border-box;
   margin: 0 auto;
 `;
 
-export const LogoDiv = styled.div`
-  padding-right: 167px;
-`;
+export const LogoDiv = styled.div``;
 
 export const CenterDiv = styled.div`
   display: flex;
-  justify-content: space-around;
 `;
 
 export const MenuDiv = styled.div`
   display: flex;
-  justify-content: space-around;
-  padding: 0 40px;
 `;
 
 export const MenuText = styled.div`
   display: flex;
-  align-items: center;
   font-size: 16px;
   line-height: 19px;
-  color: #ffffff;
-  padding: 0 14px;
 `;
 
-export const Logo = styled.img`
-  margin-left: 40px;
-`;
+export const Logo = styled.img``;
 
 export const Inst = styled(Instagram)`
-  padding: 0 12px;
+  margin-right: 12px;
 `;
 
-export const Fb = styled(Facebook)`
-  padding: 0 12px;
-`;
+export const Fb = styled(Facebook)``;
 
 export const Twi = styled(Twitter)`
-  padding: 0 12px;
+  margin-right: 12px;
 `;
 
 export const CenterText = styled.div`
@@ -67,8 +60,7 @@ export const CenterText = styled.div`
   align-items: center;
   font-size: 24px;
   line-height: 29px;
-  color: #ffffff;
-  padding: 0 20px;
+  padding: 0px 20px;
 `;
 
 export const StyledLink = styled(Link)`
