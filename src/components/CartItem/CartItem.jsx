c2b9ecd18a911e4b306b9307c07642c6ef171f50
Dovydas <dodefke@gmail.com>
2020-12-18 16:40:11 +0200

    Added cart items comp

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -4,12 +4,28 @@ import * as S from "./CartItem.style";
 function CartItem({ array }) {
   return (
     <>
-      {array &&
+      {array.length !== 0 ? (
         array.map((item) => (
           <S.Container key={item.id}>
-            <S.Image background={item.image} />
+            <S.FlexContainer>
+              <S.Image src={item.image} />
+              <S.Info>
+                <S.Title>{item.title}</S.Title>
+                <S.Size>
+                  {item.size.length > 1 ? item.size.slice(0, 1) : item.size}
+                </S.Size>
+              </S.Info>
+            </S.FlexContainer>
+            <S.FlexContainer>
+              <S.Price>{item.price / 100} €</S.Price>
+            </S.FlexContainer>
           </S.Container>
-        ))}
+        ))
+      ) : (
+        <S.Container>
+          <S.Title>Shopping cart is empty</S.Title>
+        </S.Container>
+      )}
     </>
   );
 }
--- src/components/CartItem/CartItem.style.js
+++ src/components/CartItem/CartItem.style.js
@@ -1,9 +1,53 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  border-top: 2px solid #383c43;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 0;
+  &:last-child {
+    border-bottom: 2px solid #383c43;
+  }
+`;
 
-export const Image = styled.div`
+export const Image = styled.img`
   width: 95px;
   height: 150px;
-  background: url("${(props) => props.background}");
+  object-fit: cover;
+  /* background: url("${(props) => props.background}");
+  background-repeat: no-repeat;
+  background-size: contain; */
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+`;
+
+export const Price = styled.div`
+  font-size: 40px;
+  line-height: 48px;
+  color: #383c43;
+`;
+
+export const Title = styled.div`
+  font-size: 40px;
+  line-height: 48px;
+  color: #383c43;
+`;
+
+export const Size = styled.div`
+  width: 41px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background: #383c43;
+  border-radius: 3px;
+  font-size: 24px;
+  line-height: 29px;
+`;
+
+export const Info = styled.div`
+  margin-left: 36px;
 `;
